4f79739d529163435500ce31247a3c8bf861c7d0
Cleiton <cleitonrmacedo@gmail.com>
2021-11-25 17:11:48 -0300

    feat: Requisito 1 - Implemento da funcao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  const animal = data.species.filter((specie) => ids.includes(specie.id));
+  return animal;
 }
 
 module.exports = getSpeciesByIds;
