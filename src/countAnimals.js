5a1381a7a33acc200e8e5d7f4415f26442dcda30
Cleiton <cleitonrmacedo@gmail.com>
2022-01-10 09:04:42 -0300

    feat: Requisito 5 - Implemento da funcao countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const result = {};
+  if (animal === undefined) {
+    data.species.forEach((specie) => {
+      result[specie.name] = specie.residents.length;
+    });
+    return result;
+  }
+
+  const findSpecie = data.species.find((specie) => animal.specie === specie.name);
+  const countSex = findSpecie.residents.filter((resident) => resident.sex === animal.sex).length;
+  if (animal.sex === undefined) {
+    return findSpecie.residents.length;
+  }
+
+  return countSex;
 }
 
 module.exports = countAnimals;
