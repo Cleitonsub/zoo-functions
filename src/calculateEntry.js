3b7bf6bf3bdb042115b5e1e8f98be764d21368f0
Cleiton <cleitonrmacedo@gmail.com>
2022-01-10 09:22:26 -0300

    feat: Requisito 6 - Implemento da funcao calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const childs = entrants.filter((entrant) => entrant.age < 18).length;
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const seniors = entrants.filter((entrant) => entrant.age >= 50).length;
+  return {
+    child: childs,
+    adult: adults,
+    senior: seniors,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const cE = countEntrants(entrants);
+  const { prices } = data;
+  return cE.child * prices.child + cE.adult * prices.adult + cE.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
