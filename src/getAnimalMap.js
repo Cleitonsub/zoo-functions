a4213b7fe0131f96bfc53600f9a79e307063dcd9
Cleiton <cleitonrmacedo@gmail.com>
2022-01-10 14:00:08 -0300

    fix: Requisito 7 - Correcao de erros no lint -'getAnimalWithLocation' was used before it was defined-

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,16 +1,16 @@
 const data = require('../data/zoo_data');
 
+const getAnimalWithLocation = (location) => {
+  const filter = data.species.filter((specie) => location === specie.location);
+  return filter.map((specie) => specie.name);
+};
+
 const withoutParams = () => ({
   NE: getAnimalWithLocation('NE'),
   NW: getAnimalWithLocation('NW'),
   SE: getAnimalWithLocation('SE'),
   SW: getAnimalWithLocation('SW'),
 });
-
-const getAnimalWithLocation = (location) => {
-  const filter = data.species.filter((specie) => location === specie.location);
-  return filter.map((specie) => specie.name);
-};
 
 function getAnimalMap(options) {
   if (options === undefined) {
