0b9d3f4b86d40991ca15c9924aede5aae8c8b2e8
Cleiton <cleitonrmacedo@gmail.com>
2022-01-10 09:34:58 -0300

    Requisito 7 - Parte da funcao getAnimalMap implementada

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const withoutParams = () => ({
+  NE: getAnimalWithLocation('NE'),
+  NW: getAnimalWithLocation('NW'),
+  SE: getAnimalWithLocation('SE'),
+  SW: getAnimalWithLocation('SW'),
+});
+
+const getAnimalWithLocation = (location) => {
+  const filter = data.species.filter((specie) => location === specie.location);
+  return filter.map((specie) => specie.name);
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return withoutParams();
+  }
 }
 
 module.exports = getAnimalMap;
