32d4ad10760c7d0e4957b43fedec068e7f8e177b
Cleiton <cleitonrmacedo@gmail.com>
2022-01-10 10:23:24 -0300

    feat: Requisito 8Implemento da funcao getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+const week = Object.keys(hours);
+
+const zooInfo = () => {
+  const obj = week.reduce((acc, curr) => {
+    acc[curr] = {
+      officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+      exhibition: species.filter((animal) => animal.availability.includes(curr))
+        .map((animal) => animal.name),
+    };
+    return acc;
+  }, {});
+  obj.Monday = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return obj;
+};
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) {
+    return zooInfo();
+  }
+  const check = species.some((specie) => scheduleTarget === specie.name);
+  if (!check && !week.includes(scheduleTarget)) {
+    return zooInfo();
+  }
+  if (week.includes(scheduleTarget)) {
+    return { [scheduleTarget]: zooInfo()[scheduleTarget] };
+  }
+  return species.find((specie) => specie.name === scheduleTarget).availability;
 }
 
 module.exports = getSchedule;
