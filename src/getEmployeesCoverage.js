680551ddc1d75793f1608053556d732aba63782e
Cleiton <cleitonrmacedo@gmail.com>
2022-01-13 19:14:26 -0300

    feat: Requisito 10 - Implemento da funcao getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// Este requisito foi realizado com a ajuda da nossa amiga Regiane Santos (Recfs235)
+// https://github.com/Recfs235
+
+const { species, employees } = data;
+
+const EmployeeData = (employee) => {
+  const { id, responsibleFor } = employee;
+  return {
+    id,
+    fullName: `${employee.firstName} ${employee.lastName}`,
+    species: responsibleFor
+      .map((animal) => species
+        .find((specie) => specie.id === animal).name),
+    locations: responsibleFor
+      .map((animal) => species
+        .find((specie) => specie.id === animal).location),
+  };
+};
+
+function getEmployeesCoverage(idOrName) {
+  if (idOrName === undefined) {
+    const allEmployeesData = employees.map(EmployeeData);
+    return allEmployeesData;
+  }
+  const { name, id } = idOrName;
+  const emplo = employees.find((employee) => employee.firstName === name
+  || employee.lastName === name || employee.id === id);
+
+  if (emplo === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  return EmployeeData(emplo);
 }
 
 module.exports = getEmployeesCoverage;
